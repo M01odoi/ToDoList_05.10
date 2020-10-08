b43cabfc22d72b6a0deaade9a341bf9ff5e40ba9
Molodoi <alexsveshkarev@gmail.com>
2020-10-08 14:23:15 +0300

    add button Remove and panels with count tasks

--- scr.js
+++ scr.js
@@ -1,21 +1,27 @@
 const formAddTask = document.querySelector('#formAddTask');
-
+const removeList = document.querySelector('[data-toggle="removeAll"]');
+removeList.addEventListener('click', function () {
+    const taskList = document.querySelector(`.list-group`);
+    taskList.innerHTML = '';
+    localStorage.clear();
+});
 formAddTask.addEventListener('submit', handleFormAddSubmit);
 
 function handleFormAddSubmit(event) {
     event.preventDefault();
-
     const task = {
         title: this.querySelector('[name="title"]').value,
         status: 1,
         //1. to do 2. inprog 3.done
         id: new Date().getTime()
     };
+
     localStorage.setItem(task.id, JSON.stringify(task));
     addTask(task);
     // this.querySelector('[name="title"]').value = '';
     $('#modalAddTask').modal('hide');
     this.reset();
+
 }
 
 function addTask(task) {
@@ -24,13 +30,27 @@ function addTask(task) {
     taskListItem.classList.add('list-group-item');
     taskListItem.innerText = task.title;
     taskList.appendChild(taskListItem);
-    // let $but =  $('<button>').appendTo(taskListItem);
-    // $but.text('delete');
 }
 
+let countFirst = 0, countSecond = 0, countThird = 0;
 for (let key in localStorage) {
     if (!localStorage.hasOwnProperty(key)) continue;
     const task = JSON.parse(localStorage[key]);
+    switch (task.status) {
+        case 1: countFirst++;
+        break;
+        case 2: countSecond++;
+        break;
+        case 3: countThird++;
+        break;
+            default: throw new Error('AAAAAAAAaa');
+    }
+
     addTask(task);
 }
+$('.firstSpan').text(countFirst);
+$('.secondSpan').text(countSecond);
+$('.thirdSpan').text(countThird);
+
 //localStorage, sessionStorage
+
